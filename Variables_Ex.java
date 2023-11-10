36f39b0c001ba1307673e2e4486589e47ecb2d17
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2023-11-10 18:31:11 +0530

    Day-5 Updates

--- Variables_Ex.java
+++ Variables_Ex.java
@@ -1,26 +1,24 @@
-class Variables_Ex
+class Variables_Ex  //pascal case
 {
-	int empId=23456;//global variable & instance 
-	static	short countryCode=91;//global variable & static
+
 	public static void main(String args[])
 	{
-		char gender='M';//local variables
-		byte age=127;
-		long contact=9494348734l;
-		float salary=23.54235476932243f;
-		double result=23.54235476932243;
-		boolean value=true;
-		System.out.println("welcome to java");	
-		System.out.println(gender);	
-		System.out.println(age);	
-		System.out.println(contact);	
-		System.out.println(salary);	
-		System.out.println(result);	
+		byte age=29;
+		long contact=934939432l;
+		float salary=23.56383829034534f;
+		double result=23.56383829034534;
+		boolean condition=true;
+		char gender='M';
 		
+		System.out.println(age);
+		System.out.println(contact);
+		System.out.println(salary);
+		System.out.println(result);
+		System.out.println(condition);
+		System.out.println(gender);
 		
-		Variables_Ex obj=new Variables_Ex();
-		System.out.println(Variables_Ex.countryCode);
-		System.out.println(obj.empId);
 		
 	}
+
+
 }
